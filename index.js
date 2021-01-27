2e7811f0357bbf781d40a858612352b37071fbc5
abhishekD97 <abhishekdolli@gmail.com>
2021-01-27 18:23:35 +0530

    heroku push attempt 2

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import favicon from "express-favicon";
+import path from "path";
+const app = express();
+
+app.use(favicon(__dirname+'/build/favicon.ico'));
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,'build')));
+app.get('/ping', function(req,res) {
+ return res.send('pong');    
+});
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+});
+app.listen(process.env.PORT || 8080);
